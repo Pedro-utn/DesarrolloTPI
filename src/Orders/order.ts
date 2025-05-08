95289dda91f7bd38809fcdb780637a029411e0c0
PedroMansilla <pedromansilla154@gmail.com>
2025-05-08 17:59:19 -0300

    Acomode la clase order y agregue todas las funcionalidad xd

--- src/Orders/order.ts
+++ src/Orders/order.ts
@@ -1,44 +1,51 @@
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-  } from 'typeorm';
-  
-  class Location {
-    @Column('float')
-    lat: number;
-  
-    @Column('float')
-    lng: number;
-  }
-  
-  class Address {
-    @Column()
-    street: string;
-  
-    @Column()
-    number: string;
-  
-    @Column()
-    cityId: number;
-  
-    @Column(() => Location)
-    location: Location;
-  }
-  
-  @Entity()
-  export class Order {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column()
-    userId: number;
-  
-    // TypeORM no soporta arrays de enteros de forma automática en todos los drivers
-    @Column("int", { array: true })
-    products: number[];
-  
-    @Column(() => Address) //Clase Embebida
-    location: Address;
-  }
-  
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+class Location {
+  @Column('float')
+  lat: number;
+
+  @Column('float')
+  lng: number;
+}
+
+class Address {
+  @Column()
+  street: string;
+
+  @Column()
+  number: string;
+
+  @Column()
+  cityId: number;
+
+  @Column(() => Location)
+  location: Location;
+}
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @Column()
+  restaurantId: number;
+
+  @Column("int", { array: true })
+  products: number[];
+
+  @Column(() => Address)
+  location: Address;
+
+  @Column({ default: 'pending' })
+  status: string;
+
+  @Column({ type: 'json', nullable: true })
+  delivery: any; // Podés usar una clase embebida si sabés su forma
+}
