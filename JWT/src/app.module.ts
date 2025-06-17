c27d9a384d8812a41cdcc725fbf0ee1196ff197a
Pedro-utn <pedromansilla154@gmail.com>
2025-06-17 18:22:40 -0300

    Postgres en JWT

--- JWT/src/app.module.ts
+++ JWT/src/app.module.ts
@@ -1,3 +1,4 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -10,14 +11,18 @@ import { UsersService } from './users/users.service';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      database: 'db.sql',
+      type: 'postgres', // Cambiado de 'sqlite' a 'postgres'
+      host: 'database', // El nombre del servicio de la base de datos en docker-compose.yml
+      port: 5432, // El puerto interno por defecto de PostgreSQL en el contenedor
+      username: 'postgres', // Coincide con POSTGRES_USER en docker-compose.yml
+      password: '1234', // Coincide con POSTGRES_PASSWORD en docker-compose.yml
+      database: 'db_jwt', // Nombre de la base de datos (ver nota importante abajo)
       entities,
-      type: 'sqlite',
-      synchronize: true,
+      synchronize: true, // Esto mantendrá la creación automática de tablas
     }),
     TypeOrmModule.forFeature(entities),
   ],
-  controllers: [AppController,UsersController],
+  controllers: [AppController, UsersController],
   providers: [AuthGuard, JwtService, UsersService],
 })
 export class AppModule {}
